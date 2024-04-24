b98cad6f913cf073287783c1049777b8c67fcc8f
aniketshisath5648 <162993508+aniketshisath5648@users.noreply.github.com>
2024-04-24 09:39:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var menubutton = document.querySelector('.anii');
+var midnav = document.querySelector('.mid-nav');
+
+menubutton.addEventListener("click", function() {
+    midnav.classList.toggle('down');
+})
+
+
+
